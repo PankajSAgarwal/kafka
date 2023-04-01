e7d3626ddc572eeb76b678527bc208ca7fbffb23
Pankaj Agarwal <2005pank@gmail.com>
2023-04-01 21:57:49 +0530

    Created kafka consumer

--- kafka-beginner-course/kafka-consumer-opensearch/src/main/java/com/pankaj/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-beginner-course/kafka-consumer-opensearch/src/main/java/com/pankaj/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -7,6 +7,9 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.opensearch.client.*;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
@@ -15,6 +18,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Properties;
 
 
 public class OpenSearchConsumer {
@@ -44,15 +48,30 @@ public class OpenSearchConsumer {
         }
         return restHighLevelClient;
     }
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "consumer-opensearch-demo";
+
+        // create consumer config
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
+
+        //create consumer
+        return new KafkaConsumer<String, String>(properties);
+    }
     public static void main(String[] args) throws IOException {
         Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
 
         // first create opensearch client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
         // create kafka client
-        
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
         // we need to create the index on OpenSearch if it doesn't already exist
-        try(openSearchClient){
+        try(openSearchClient;consumer){
             boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
             if(!indexExists){
                 CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
@@ -73,4 +92,6 @@ public class OpenSearchConsumer {
         // close things
 
     }
+
+
 }

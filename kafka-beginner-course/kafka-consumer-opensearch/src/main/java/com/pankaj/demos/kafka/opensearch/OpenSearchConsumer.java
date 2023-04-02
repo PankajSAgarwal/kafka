bcd55cd49778a988e7e6b7ab3e9be91c8cbd3f28
Pankaj Agarwal <2005pank@gmail.com>
2023-04-02 10:53:12 +0530

    Making opensearch consumer idempotent by adding id to indexrequest. Id is extracted from record json value using gson library

--- kafka-beginner-course/kafka-consumer-opensearch/src/main/java/com/pankaj/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-beginner-course/kafka-consumer-opensearch/src/main/java/com/pankaj/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,12 +1,12 @@
 package com.pankaj.demos.kafka.opensearch;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -14,7 +14,9 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.index.IndexResponse;
-import org.opensearch.client.*;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
 import org.opensearch.common.xcontent.XContentType;
@@ -29,7 +31,7 @@ import java.util.Properties;
 
 
 public class OpenSearchConsumer {
-    public static RestHighLevelClient createOpenSearchClient(){
+    public static RestHighLevelClient createOpenSearchClient() {
         String connString = "http://localhost:9200";
         // we build a URI from the connection string
         RestHighLevelClient restHighLevelClient;
@@ -55,21 +57,35 @@ public class OpenSearchConsumer {
         }
         return restHighLevelClient;
     }
+
     private static KafkaConsumer<String, String> createKafkaConsumer() {
         String bootstrapServers = "127.0.0.1:9092";
         String groupId = "consumer-opensearch-demo";
 
         // create consumer config
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
         //create consumer
         return new KafkaConsumer<String, String>(properties);
     }
+
+    private static String extractId(String json) {
+
+        //gson library
+        return JsonParser
+                .parseString(json)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
+    }
+
     public static void main(String[] args) throws IOException {
         Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
 
@@ -78,41 +94,42 @@ public class OpenSearchConsumer {
         // create kafka client
         KafkaConsumer<String, String> consumer = createKafkaConsumer();
         // we need to create the index on OpenSearch if it doesn't already exist
-        try(openSearchClient;consumer){
+        try (openSearchClient; consumer) {
             boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
-            if(!indexExists){
+            if (!indexExists) {
                 CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
                 openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
                 log.info("Wikimedia Index has been created.");
-            }else{
+            } else {
                 log.info("Wikimedia index already exists");
             }
 
             // we subscribe the consumer
             consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
 
-            while (true){
+            while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
                 int recordCount = records.count();
                 log.info("Received " + recordCount + " record(s).");
                 for (ConsumerRecord<String, String> record : records) {
-                    try{
-                        IndexRequest indexRequest = new IndexRequest("wikimedia").source(record.value(), XContentType.JSON);
+                    // send the record in opensearch
+                    //strategy 1: Define an id using Kafka record coordinates
+                    //String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+                    // Strategy 2:We extract the id from the record json value
+                    String id = extractId(record.value());
+
+                    try {
+                        IndexRequest indexRequest = new IndexRequest("wikimedia")
+                                .source(record.value(), XContentType.JSON)
+                                .id(id);
                         IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
                         log.info(response.getId());
-                    }catch (Exception e){
-                        
+                    } catch (Exception e) {
 
                     }
-
                 }
-
             }
-
         }
-
-
-
 
 
         // main code logic
